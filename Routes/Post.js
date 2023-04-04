c5450298f357857a879cc8b20b71fdbecb64b858
Thiruvaran Thayalan <110049957+T-Thiru@users.noreply.github.com>
2023-04-04 10:57:20 +0200

    setting up endpoints

--- Routes/Post.js
+++ Routes/Post.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/post", async (req, res) => {
-  res.json({ message: "post" });
+router.get("/", async (req, res) => {
+  res.json({ message: "GET" });
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  res.json({ message: "POST" });
+});
+
+router.put("/", async (req, res) => {
+  console.log(req.params);
+  res.json({ message: "PUT" });
+});
+
+router.delete("/", async (req, res) => {
+  console.log(req.params);
+  res.json({ message: "DELETE" });
+});
+
+router.patch("/like-post/:id", async (req, res) => {
+  console.log(req.params);
+  res.json({ message: "LIKE" });
+});
+
+router.patch("/dislike-post/:id", async (req, res) => {
+  console.log(req.params);
+  res.json({ message: "DISLIKE" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.use();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./Routes/Post"));
 
 // server is listening on port
 app.listen(port, () => console.log("Server has started on port " + port));
